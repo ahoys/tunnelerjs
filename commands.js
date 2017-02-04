95c9c52fff02594af5fb73e88daaf0445119dea4
Ari Höysniemi <ari.hoysniemi@gmail.com>
2017-02-04 14:43:02 +0200

    Select from clients feature.

--- commands.js
+++ commands.js
@@ -29,5 +29,64 @@ module.exports = (client) => {
         Message.reply(`pong in ${client.ping} ms. `);
     };
 
+    /**
+     * Selects random users from a guild and prints them.
+     * @param Message
+     * @param container
+     * @returns {boolean}
+     */
+    module.selectFromClients = (Message, container) => {
+        if (!isNaN(container.str) && container.str > 0) {
+            const memberArr = Message.guild.members.map((member) => {return member.displayName});
+            let i = 0;
+            let members = '';
+            module.shuffle(memberArr).forEach((displayName) => {
+                if (i < Number(container.str) && displayName !== undefined) {
+                    members += i > 0 ? `, ${displayName}` : displayName;
+                    i++;
+                }
+            });
+            if (i > 0) {
+                // Success!
+                members = i === 1 ? members.replace(',', '') : members;
+                const msg = `${Strings.selectFromClients.success_0}` +
+                    `${Strings.selectFromClients.name_strings[Math.floor(Math.random() 
+                        * Strings.selectFromClients.name_strings.length)]}` +
+                    `${Strings.selectFromClients.success_1} `;
+                Message.channel.send(`${msg}${members}.`);
+                return true;
+            } else {
+                // Print failed attempt (guild is empty).
+                Message.channel.send(Strings.selectFromClients.fail_2);
+            }
+        } else if (container.str === '0') {
+            // Print failed attempt (trying to pick from zero).
+            Message.reply(Strings.selectFromClients.fail_1);
+        } else {
+            // Print failed attempt (invalid command).
+            Message.reply(Strings.selectFromClients.fail_0);
+        }
+        return false;
+    };
+
+    /**
+     * Shuffles an array.
+     * @param array
+     * @returns {*}
+     */
+    module.shuffle = (array) => {
+        if (array.constructor === Array) {
+            let temp, current, top = array.length;
+            if (top) while (--top) {
+                current = Math.floor(Math.random() * (top + 1));
+                temp = array[current];
+                array[current] = array[top];
+                array[top] = temp;
+            }
+            return array;
+        }
+        return [];
+    };
+
     return module;
 };

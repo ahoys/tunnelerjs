713e1138c6d51e7ad38392c9382fa4d250d67842
Ari Höysniemi <ari.hoysniemi@gmail.com>
2017-02-04 20:51:09 +0200

    Initial anti spam functionality.

--- tunneler.js
+++ tunneler.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 const Auth = require("./config/auth.json");
 const Commands = require('./commands')(client);
 const Util = require('./util')();
+const Strings = require('./config/strings.json');
 
 client.login(Auth.token);
 
@@ -10,7 +11,7 @@ client.login(Auth.token);
  * An initial connection handler.
  */
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.username}, serving ${client.guilds.array().length} servers.`);
+    console.log(`Logged in as ${client.user.username}, serving ${client.guilds.array().length} server(s).`);
 });
 
 /**
@@ -25,6 +26,31 @@ client.on('disconnected', () => {
  * The main message handler.
  */
 client.on('message', Message => {
+    // Load guild settings.
+    const settingsContainer = Util.getGuildSettings(Message);
+
+    // Handle spamming.
+    if (
+        Boolean(settingsContainer.enable_anti_spam_filtering) &&
+        Util.isSpam(Message, settingsContainer)
+    ) {
+        const user = Message.member;
+        if (!settingsContainer['enable_quiet_mode']) {
+            Message.channel.send(`${Strings.action_ban.spam.success_0} ` +
+                `${user.user.username}` +
+                ` ${Strings.action_ban.spam.success_1}`);
+        }
+
+        if (user.bannable) {
+            const ban = user.ban(1);
+            ban.then((val) => {
+                console.log(`Successfully banned: ${user.username}`);
+            }).catch((reason) => {
+                console.log('Ban failed: ', reason);
+            });
+        }
+    }
+
     const commandContainer = Util.handleMessage(Message);
     if (commandContainer.hasOwnProperty('cmd')) {
         // Message should always be provided, commandContainer should be used if you are about to
--- util.js
+++ util.js
@@ -1,8 +1,95 @@
 const Auth = require('./config/auth.json');
 const CommandsStr = require('./config/commands.json');
+const Settings = require('./config/settings.json');
+const immutable = require('immutable');
+
 module.exports = () => {
 
     const module = {};
+    let monitoredClients = new immutable.Map({});
+
+    /**
+     * Returns settings for a guild.
+     * @param Message
+     * @returns {{enable_anti_spam_filtering: boolean, enable_quiet_mode: boolean, enable_client_commands: boolean, max_repeat_of_message: number, max_urls_in_message: number, max_identical_urls_in_message: number, operators: Array}}
+     */
+    module.getGuildSettings = (Message) => {
+        const guild = Message.guild;
+        return Settings.guilds.hasOwnProperty(guild.id)
+            ? Settings.guilds[guild.id]
+            : {
+                "enable_anti_spam_filtering": true,
+                "enable_quiet_mode": false,
+                "enable_client_commands": false,
+                "max_repeat_of_message": 8,
+                "max_urls_in_message": 4,
+                "max_identical_urls_in_message": 2,
+                "operators": []
+            } ;
+    };
+
+    /**
+     * Occurrence counter.
+     * @param array
+     * @param search
+     * @returns {any|number|*|R}
+     */
+    module.getOccurrences = (array, search) => {
+        return array.reduce((n, value) => {
+            return n + (value === search);
+        }, 0);
+    };
+
+    /**
+     * A very simplistic spam detector.
+     * @param Message
+     * @param settingsContainer
+     * @returns {boolean}
+     */
+    module.isSpam = (Message, settingsContainer) => {
+        let spam = false;
+        // Attempt to recognize spam from a singular message.
+        const words = Message.content.split(" ");
+        let urlCountInMessage = 0;
+        const urls = [];
+        words.forEach((word) => {
+            if (new RegExp("([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?").test(word)) {
+                urls.push(word);
+                urlCountInMessage++;
+                if (module.getOccurrences(urls, word) > settingsContainer['max_identical_urls_in_message']) {
+                    spam = true;
+                }
+            }
+        });
+        if (urlCountInMessage > settingsContainer['max_urls_in_message']) {
+            spam = true;
+        }
+
+        // Attempt to recognize spam from message history.
+        const authorId = Message.author.id;
+        const content = encodeURIComponent(Message.content);
+        let messageHistory = monitoredClients.get(authorId);
+        if (monitoredClients.has(authorId)) {
+            // An existing user.
+            const count = module.getOccurrences(messageHistory, content);
+            if (count > settingsContainer['max_repeat_of_message']) {
+                spam = true;
+            }
+            messageHistory.push(content);
+        } else {
+            // A new user to be monitored.
+            messageHistory = [content];
+        }
+        // Empty array now and then to avoid buffer overloads.
+        if (messageHistory.length > settingsContainer['max_repeat_of_message'] * 2) {
+            messageHistory = [content];
+        }
+        
+        // Update memory.
+        monitoredClients = monitoredClients.set(authorId, messageHistory);
+
+        return spam
+    };
 
     /**
      * A simple message handler.

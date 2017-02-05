322c0a5084a45dfa7f00af8c8dd20d813b693186
Ari Höysniemi <ari.hoysniemi@gmail.com>
2017-02-05 11:40:39 +0200

    Mention execution.

--- commands/module.inc.mention.js
+++ commands/module.inc.mention.js
@@ -1,9 +1,15 @@
+const Strings = require('../config/strings.json');
 module.exports = () => {
 
     const module = {};
 
-    module.execute = () => {
-        console.log('mention');
+    /**
+     * Executes a mention message.
+     * @param messageObject
+     */
+    module.execute = (messageObject) => {
+        messageObject.channel.send(Strings.mention.success_0[Math.floor(Math.random() *
+            Strings.mention.success_0.length)]);
     };
 
     return module;

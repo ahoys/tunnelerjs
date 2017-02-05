de0c006679da57c986b36f7a44e6f7f2af9940a5
Ari Höysniemi <ari.hoysniemi@gmail.com>
2017-02-05 11:37:49 +0200

    Added commentary.

--- commands/module.inc.about.js
+++ commands/module.inc.about.js
@@ -3,6 +3,10 @@ module.exports = () => {
 
     const module = {};
 
+    /**
+     * Executes about string.
+     * @param messageObject
+     */
     module.execute = (messageObject) => {
         messageObject.channel.send(Strings.about.success_0);
     };

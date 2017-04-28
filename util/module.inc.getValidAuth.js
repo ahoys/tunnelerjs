56ee8c7a82ebf36d0be5e19b515def0e0908c4d5
Ari Höysniemi <ari.hoysniemi@gmail.com>
2017-04-28 18:43:29 +0300

    Commentary and regex.

--- util/module.inc.getValidAuth.js
+++ util/module.inc.getValidAuth.js
@@ -1,3 +1,7 @@
+/**
+ * Verifies that the authentication files are set properly.
+ * 1) auth.json exists, 2) token given, 3) id given, 4) owner given.
+ */
 const fs = require('fs');
 module.exports = () => {
     if (!fs.existsSync('./config/auth.json')) {
@@ -9,11 +13,12 @@ module.exports = () => {
         console.log('[AUTH ERROR] Invalid token given.');
         process.exit(1);
     }
-    if (typeof Auth.id !== 'string') {
+    const reg = new RegExp(/^\d+$/);
+    if (typeof Auth.id !== 'string' || !reg.test(Auth.id)) {
         console.log('[AUTH ERROR] Invalid id given.');
         process.exit(1);
     }
-    if (typeof Auth.owner !== 'string') {
+    if (typeof Auth.owner !== 'string' || !reg.test(Auth.owner)) {
         console.log('[AUTH ERROR] Invalid owner given.');
         process.exit(1);
     }

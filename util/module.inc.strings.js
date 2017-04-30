65e3f8cd4e99865ed14d96250d96d0c383b8ecb1
Ari Höysniemi <ari.hoysniemi@gmail.com>
2017-04-30 17:44:20 +0300

    Simplify.

--- util/module.inc.strings.js
+++ util/module.inc.strings.js
@@ -4,58 +4,31 @@
 const fs = require('fs');
 module.exports = (Debug, lang = 'en') => {
     const module = {};
+
     // Make sure strings.json exists.
     if (!fs.existsSync('./config/strings.json')) {
         Debug.print('config/strings.json is missing. The process will now exit.', 'STRINGS CRITICAL');
         process.exit(1);
     }
+
     // Load the resource file.
     const StringsJSON = require('../config/strings.json');
-    Debug.print('Strings file strings.json loaded.', 'STRINGS');
     // Make sure a valid language is given.
     if (typeof lang !== 'string' || lang.length < 1) {
         Debug.print('Invalid localization parameter given. The process will now exit.', 'STRINGS CRITICAL');
         process.exit(1);
     }
-    /**
-     * Returns the name of the application.
-     */
-    module.name = (loc = lang) => {
-        try {
-            return StringsJSON.name.loc || StringsJSON.name.default;
-        } catch (e) {
-            Debug.print('Returning name failed.', 'STRINGS ERROR', true, e);
-            return '';
-        }
-    }
-    /**
-     * Returns the version of the application.
-     */
-    module.version = (loc = lang) => {
-        try {
-            return StringsJSON.version.loc || StringsJSON.version.default;
-        } catch (e) {
-            Debug.print('Returning version failed.', 'STRINGS ERROR', true, e);
-            return '';
-        }
-    }
+    
+    Debug.print('Strings file strings.json loaded.', 'STRINGS');
+
     /**
      * Returns a localized string.
      */
     module.get = (strArr, loc = lang) => {
         try {
-            if (typeof strArr === 'string') {
-                // The string(s) should be presented in an array.
-                strArr = [strArr];
-            }
-            if (strArr.constructor === Array) {
-                // Construct the full path and find the string.
-                const path = strArr.reduce((o, n) => o[n], StringsJSON);
-                return path.loc || path.default;
-            } else {
-                Debug.pring('Invalid string type.', 'STRINGS WARN');
-                return '';
-            }
+            // Construct the full path and find the string.
+            const path = strArr.reduce((o, n) => o[n], StringsJSON);
+            return path.loc || path.default;
         } catch (e) {
             Debug.print('Returning string failed.', 'STRINGS ERROR', true, e);
             return '';

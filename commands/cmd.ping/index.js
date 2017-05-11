5155ba97cd0a6bd66a6fdaea60fcead769a87b77
Ari Höysniemi <ari.hoysniemi@gmail.com>
2017-05-11 20:39:36 +0300

    Lint.

--- commands/cmd.ping/index.js
+++ commands/cmd.ping/index.js
@@ -1,15 +1,21 @@
 /**
  * Replies with a client ping.
- * 
+ *
  * Author: Ari Höysniemi
  * Date: May 6. 2017
+ * @param {object} Debug
+ * @param {object} Settings
+ * @param {object} Strings
+ * @param {string} name
+ * @return {object}
  */
 module.exports = (Debug, Settings, Strings, name) => {
     const module = {};
 
     /**
      * Returns a client ping.
-     * @returns {number}
+     * @param {object} Client
+     * @return {number}
      */
     const getPing = (Client) => {
         return Math.round(Client.ping);
@@ -17,18 +23,19 @@ module.exports = (Debug, Settings, Strings, name) => {
 
     /**
      * Executes the command.
-     * @param {object} Message : Discord.js Message object.
-     * @returns {string} : The string that will be used as a reply.
+     * @param {object} Message
+     * @return {string}
      */
     module.execute = (Message) => {
         try {
             const ping = getPing(Message.client);
             return `${Strings['success_0']}${ping}${Strings['success_1']}`;
         } catch (e) {
-            Debug.print(`Executing a command (${name}) failed.`, `${name} ERROR`, true, e);
-            return '';
+            Debug.print(`Executing a command (${name}) failed.`,
+            `${name} ERROR`, true, e);
         }
-    }
+        return '';
+    };
 
     return module;
-}
+};

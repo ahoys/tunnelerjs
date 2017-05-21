5761840419300a8d30dd9e16d158b1253a0b1449
Ari Höysniemi <ari.hoysniemi@gmail.com>
2017-05-21 16:19:40 +0300

    Added guild file creation support.

--- handles/on.ready/handle.js
+++ handles/on.ready/handle.js
@@ -1,4 +1,5 @@
 const {print} = require('../../util/module.inc.debug')();
+const fs = require('fs');
 
 /**
  * Ready handle.
@@ -9,17 +10,66 @@ const {print} = require('../../util/module.inc.debug')();
 module.exports = (Client, GuildsMap) => {
     const module = {};
 
+    const createGuildFiles = (guildId) => {
+        try {
+            const path = `./guilds/${guildId}`;
+            if (!fs.existsSync(path)) {
+                fs.mkdirSync(path);
+                const template = '' +
+                '{\n' +
+                '    "localization": "en",\n' +
+                '    "middlewares": {\n' +
+                '      "antispam": {\n' +
+                '        "enabled": true\n' +
+                '      }\n' +
+                '    },\n' +
+                '    "commands": {\n' +
+                '      "exit": {\n' +
+                '        "enabled": true,\n' +
+                '        "access": ["owner"]\n' +
+                '      }\n' +
+                '    }\n' +
+                '}\n';
+                fs.writeFileSync(`${path}/guild.json`, template);
+                return true;
+            }
+            return false;
+        } catch (e) {
+            print(`Creating guild files failed for (${guildId}). `
+            + `The process will now exit.`,
+            'MAIN ERROR', true, e);
+            process.exit(1);
+            return false;
+        }
+    };
+
     /**
      * Executes the handle.
      */
     module.handle = () => {
         const {user, guilds} = Client;
+        let shutDown = false;
         guilds.forEach((guild) => {
             if (!GuildsMap[guild.id]) {
                 print(`Could not find configuration for a guild `
-                + `(${guild.id}).`, 'MAIN');
+                + `(${guild.id}). Generating files...`, 'MAIN');
+                if (createGuildFiles(guild.id)) {
+                    // Success.
+                    shutDown = true;
+                } else {
+                    // Fail.
+                    print('Could not create files. Create the files manually.',
+                    'MAIN WARN');
+                }
             }
         });
+        if (shutDown) {
+            print('New guild files added. Make sure they are configured '
+            + 'properly before restarting.', 'MAIN');
+            print('The process will now exit', 'MAIN', true,
+            'User must validate the new files.');
+            process.exit(0);
+        }
         print(`Serving ${guilds.array().length} server(s).`, 'MAIN');
         print(`Logged in as ${user.username}.`, 'MAIN');
         print(`Initialization ready, monitoring activity...`, 'MAIN');

982332e73ed16902e3a997aed6ccabc09e27e266
Ari Höysniemi <ari.hoysniemi@gmail.com>
2017-05-21 15:44:40 +0300

    Broke a long line.

--- handles/index.js
+++ handles/index.js
@@ -34,7 +34,8 @@ module.exports = (AuthMap, GuildsMap) => {
      * This handle has a middleware support. Middlewares are
      * executed before the commands.
      */
-    const onMessage = require('./on.message/handle.js')(Client, GuildsMap, AuthMap.owner);
+    const onMessage = require('./on.message/handle.js')(
+        Client, GuildsMap, AuthMap.owner);
     Client.on('message', (Message) => {
         try {
             new Promise((resolve, reject) => {

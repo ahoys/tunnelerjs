d0a9a8dce6b63765e0718d39ea44112f745d6b78
Ari Höysniemi <ari.hoysniemi@gmail.com>
2017-06-11 11:02:28 +0300

    5 digits arrays.

--- commands/mw.antispam/tests/test.isSpamming.js
+++ commands/mw.antispam/tests/test.isSpamming.js
@@ -5,21 +5,21 @@ let Analyse;
 
 exports.notSpam = (test) => {
     Analyse = require('../parts/module.inc.analyse')();
-    const result = Analyse.isSpamming([0, 0, 0, 0], 100);
+    const result = Analyse.isSpamming([0, 0, 0, 0, 0], 100);
     test.equal(result, false, result);
     test.done();
 }
 
 exports.isSpam0 = (test) => {
     Analyse = require('../parts/module.inc.analyse')();
-    const result = Analyse.isSpamming([0, 0.5, 0, 0], 100);
+    const result = Analyse.isSpamming([0, 0.5, 0, 0, 0], 100);
     test.equal(result, true, result);
     test.done();
 }
 
 exports.isSpam1 = (test) => {
     Analyse = require('../parts/module.inc.analyse')();
-    const result = Analyse.isSpamming([0, 1, 0, 0], 100);
+    const result = Analyse.isSpamming([0, 1, 0, 0, 0], 100);
     test.equal(result, true, result);
     test.done();
 }

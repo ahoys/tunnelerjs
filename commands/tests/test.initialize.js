f7896132a5f4e9c03ce88d2101f42bda43fa9f22
Ari Höysniemi <ari.hoysniemi@gmail.com>
2017-06-16 22:21:48 +0300

    Added tests for initializing module frames.

---
+++ commands/tests/test.initialize.js
@@ -0,0 +1,26 @@
+// nodeunit commands/tests/test.initialize.js
+
+let Commands;
+
+const cmdTemplate = require('../cmd.ping/command.json');
+cmdTemplate.jsPath = './commands/cmd.ping/index.js';
+const mwTemplate = require('../mw.antispam/middleware.json');
+mwTemplate.jsPath = './commands/mw.antispam/index.js';
+
+exports.readCmdSetup = (test) => {
+    Commands = require('../index')();
+    const result = Commands.initialize();
+    test.deepEqual(
+        result.cmdMap.ping,
+        cmdTemplate, result);
+    test.done();
+}
+
+exports.readMwSetup = (test) => {
+    Commands = require('../index')();
+    const result = Commands.initialize();
+    test.deepEqual(
+        result.mwMap.antispam,
+        mwTemplate, result);
+    test.done();
+}

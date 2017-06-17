01743aced0afabc3073ca2ebda6aa0d09a3d2ffc
Ari Höysniemi <ari.hoysniemi@gmail.com>
2017-06-17 11:36:24 +0300

    Added tests and fixed syntax errors.

--- commands/module.inc.validate.js
+++ commands/module.inc.validate.js
@@ -2,10 +2,14 @@ const {print, log} = require('../util/module.inc.debug')();
 
 module.exports = () => {
     const module = {};
+    const allowedTypes = ['string', 'boolean', 'number', 'array', 'object'];
 
-    module.isValidType = (subject) => {
+    const isValidType = (subject) => {
         try {
-            return true;
+            if (allowedTypes.indexOf(subject) >= 0) {
+                return true;
+            };
+            print(`Not supported type (${subject}).`, 'validate');
         } catch (e) {
             print(`Validating type failed (${subject})`,
                 'validate', true, e);
@@ -13,9 +17,12 @@ module.exports = () => {
         return false;
     }
 
-    module.isValidIsRequired = (subject) => {
+    const isValidIsRequired = (subject) => {
         try {
-            return true;
+            if (typeof subject === 'boolean') {
+                return true;
+            }
+            print(`Not supported isRequired (${subject}).`, 'validate');
         } catch (e) {
             print(`Validating isRequired failed (${subject})`,
                 'validate', true, e);
@@ -23,9 +30,22 @@ module.exports = () => {
         return false;
     }
 
-    module.isValidDefaultValue = (subject, type) => {
+    const isValidDefaultValue = (subject, type) => {
         try {
-            return true;
+            if (isValidType(type)) {
+                if (
+                    type === 'array' &&
+                    typeof subject === 'object' &&
+                    subject.constructor === Array
+                ) {
+                    return true;
+                } else if (typeof subject === type) {
+                    return true;
+                }
+                print(`Not supported default value (${subject}).`, 'validate');
+                return false;
+            }
+            print(`Not supported type (${type}).`, 'validate');
         } catch (e) {
             print(`Validating default value failed (${subject}, ${type})`,
                 'validate', true, e);
@@ -40,26 +60,29 @@ module.exports = () => {
             // eg. mySetting.
             Object.keys(guildSettings).forEach((key) => {
                 const settingFrame = {};
+                let i = 0;
                 // Go through all setting parameters.
                 // Eg. type, isRequired and defaultValue.
                 Object.keys(guildSettings[key]).forEach((pKey) => {
                     const thisParameter = guildSettings[key][pKey];
-                    let i;
                     switch (pKey) {
                         case 'type':
-                            if (this.isValidType(thisParameter)) {
+                            if (isValidType(thisParameter)) {
                                 settingFrame[pKey] = thisParameter;
                                 i = i + 1;
                             }
                             break;
                         case 'isRequired':
-                            if (this.isValidIsRequired(thisParameter)) {
+                            if (isValidIsRequired(thisParameter)) {
                                 settingFrame[pKey] = thisParameter;
                                 i = i + 1;
                             }
                             break;
                         case 'defaultValue':
-                            if (this.isValidDefaultValue(thisParameter)) {
+                            if (
+                                isValidDefaultValue(
+                                    thisParameter, guildSettings[key]['type'])
+                            ) {
                                 settingFrame[pKey] = thisParameter;
                                 i = i + 1;
                             }

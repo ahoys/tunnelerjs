d4acd85d5ca25be801ef4841e9c44388ba4699ec
Ari Höysniemi <ari.hoysniemi@gmail.com>
2017-10-07 10:07:58 +0300

    Added cmd.uptime.

---
+++ commands/cmd.uptime/index.js
@@ -0,0 +1,39 @@
+const {print} = require('../../util/module.inc.debug')();
+
+/**
+ * Replies with a bot uptime.
+ * Reboot does not clear the timer.
+ * 
+ * Author: Ari Höysniemi
+ * Data: October 7. 2017
+ */
+module.exports = (Settings, Strings, name) => {
+    const module = {};
+
+    /**
+     * Executes the command.
+     * @return {string}
+     */
+    module.execute = () => {
+        try {
+          const uptime = process.uptime();
+          if (uptime < 60) {
+            // Seconds
+            return `${Strings['success_0']} ${process.uptime().toFixed(2)} ${Strings['seconds']}.`;
+          } else if (uptime >= 60 && uptime < 3600) {
+            // Minutes
+            return `${Strings['success_0']} ${(process.uptime() / 60).toFixed(2)} ${Strings['minutes']}.`;
+          } else if (uptime >= 3600 && uptime < 68400) {
+            // Hours
+            return `${Strings['success_0']} ${(process.uptime() / 3600).toFixed(2)} ${Strings['hours']}.`;
+          }
+          // Days
+          return `${Strings['success_0']} ${(process.uptime() / 68400).toFixed(2)} ${Strings['days']}.`;
+        } catch (e) {
+            print(`Command execution failed.`, name, true, e);
+        }
+        return '';
+    };
+
+    return module;
+};
